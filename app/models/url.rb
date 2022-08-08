5c0ab9b8cc6dc0ce6b4333cf3804099800c4509d
Mostafa Abdelhamed <mostafa.hosny2015@hotmail.com>
2022-08-08 17:36:56 +0200

    model validations

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 class Url
   include Mongoid::Document
   include ActiveModel::Validations
-  
+
   field :original_url, type: String
   field :short_id, type: String
 
   index({ short_id: 1 }, { unique: true })
+  validates :short_id, uniqueness: { message: 'Short_id should be uniqe' }
+  validates :original_url, presence: { message: 'Url can not be empty' },
+                           format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp}\z/ }
 end
